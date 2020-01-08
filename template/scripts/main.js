4e45597f96df3733aaea5c059f54dec03104c957
Johnson Samuel <51086836+samsology@users.noreply.github.com>
2020-01-08 01:02:46 +0100

    Add files via upload

---
+++ template/scripts/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $nav = $('.nav');
+  $toggleCollapse=$('.toggle-collapse');
+  
+
+
+
+  $toggleCollapse.click(function(){
+    $nav.toggleClass('collapse');
+  })
+})
